98a767a62d9d641f061721723d5846fc0bea3205
AlexaGTorres <alexa.torres122694@gmail.com>
2022-12-11 22:28:54 -0800

    Initial commit

---
+++ app/src/main/java/com/example/planner/StartingScreen.java
@@ -0,0 +1,24 @@
+package com.example.planner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class StartingScreen extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        getSupportActionBar().hide();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starting_screen);
+    }
+
+    public void onClickCalender (View v)
+    {
+        startActivity(new Intent(MainActivity.this, Calender.class));
+    }
+}
