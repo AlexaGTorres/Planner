eefdcb76dddbe4da872b518df8f52351c4478ed1
AlexaGTorres <alexa.torres122694@gmail.com>
2022-12-14 16:21:16 -0800

    Somehow got date picker to work. :)

--- app/src/main/java/com/example/planner/Calender.java
+++ app/src/main/java/com/example/planner/Calender.java
@@ -2,16 +2,25 @@ package com.example.planner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.Calendar;
 import java.util.Date;
 
 public class Calender extends AppCompatActivity
 {
+    TextView etDate;
+    DatePickerDialog.OnDateSetListener setListener;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -20,10 +29,36 @@ public class Calender extends AppCompatActivity
         setContentView(R.layout.activity_calender);
 
         Date currentTime = Calendar.getInstance().getTime();//Get current date and time for calender
-    }
 
-    public void onClickListFromCalender (View v)
-    {
-        startActivity(new Intent(Calender.this, List.class));
+        etDate = findViewById(R.id.datePicker);
+
+        Calendar c = Calendar.getInstance();
+        final int year = c.get(Calendar.YEAR);
+        final int month = c.get(Calendar.MONTH);
+        final int day = c.get(Calendar.DAY_OF_MONTH);
+
+        etDate.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        Calender.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth
+                        ,setListener, year, month, day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+            }
+        });
+
+        setListener = new DatePickerDialog.OnDateSetListener()
+        {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth)
+            {
+                month = month + 1;
+                String date = month + "/" + dayOfMonth +"/" + year;
+                etDate.setText(date);
+            }
+        };
     }
 }
